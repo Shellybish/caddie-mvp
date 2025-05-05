eca23f9d032c8934ee86c13dfce0e6b72a3b8847
Shellybish <sheldsbishop@gmail.com>
2025-05-05 18:00:37 +0200

    Fix root page to include MainLayout with header and footer

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import HomePage from './(main)/page';
+import MainLayout from './(main)/layout';
+
+export default function RootPage() {
+  return (
+    <MainLayout>
+      <HomePage />
+    </MainLayout>
+  );
+} 
